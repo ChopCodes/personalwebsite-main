5483d0e9895abaad6c9d4f47f89c25707a2a9e38
Avinash <74913043+AvinashWarrior@users.noreply.github.com>
2022-04-10 12:27:25 +0530

    Added New Things

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 align="center">Personal Website For Myself</h1>
+<h2>This Project is still in progress, Once its done readme.md will also complete</h2>
+<p>Contributed by <a href="https://github.com/tejasbaid">TejasBaid</a>
---
+++ script.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SnowStorm from 'react-snowstorm';
+const { default: snowstorm } = require('react-snowstorm/lib/snowstorm');
+
+class hero extends hero {
+    render(){
+        return (
+            <div>
+                <SnowStorm />
+            </div>
+        )
+    }
+}
